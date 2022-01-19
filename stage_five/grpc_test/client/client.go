38175e28e49aef378b2268c360463c9b6115c5ad
SeasonPilot <pilot1@foxmail.com>
2022-01-19 23:25:12 +0800

    feat: 练习: grpc_test   4-6 go下grpc快速体验

---
+++ stage_five/grpc_test/client/client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"development/stage_five/grpc_test/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// 拨号
+	conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure())
+	//grpc.Dial(":9950", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// new client 对象
+	c := proto.NewGreeterClient(conn)
+
+	// 调用服务
+	req := proto.HelloRequest{
+		Name: "kk",
+	}
+	reply, err := c.SayHello(context.Background(), &req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(reply)
+}
