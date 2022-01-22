1df3ee0d1e2d7ef138c36e84cb118326586203ba
SeasonPilot <pilot1@foxmail.com>
2022-01-22 17:44:41 +0800

    fix: copy from grpc_test

---
+++ stage_five/grpc_error_test/server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"development/stage_five/grpc_error_test/proto"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct{}
+
+func (s *Server) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{
+		Massage: "Hello," + req.Name,
+	}, nil
+}
+
+func main() {
+	// 监听
+	listener, err := net.Listen("tcp", "0.0.0.0:1234")
+	if err != nil {
+		panic(err)
+	}
+
+	// 注册服务
+	g := grpc.NewServer()
+	proto.RegisterGreeterServer(g, &Server{})
+	// 启动服务
+	err = g.Serve(listener)
+	if err != nil {
+		panic(err)
+	}
+
+}
