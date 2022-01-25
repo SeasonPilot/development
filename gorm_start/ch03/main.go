d4f583be7602d8e54c0b7379bf2c8222670e58ce
SeasonPilot <pilot1@foxmail.com>
2022-01-25 17:09:25 +0800

    fix: copy from ch02

---
+++ gorm_start/ch03/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	ID   uint   `gorm:"primarykey"`
+	Name string `gorm:"column:user_name;type:varchar(50);index:idx_user_name;default:'bobby'"`
+}
+
+func main() {
+	dsn := "golang:golang@2020@tcp(172.19.30.30:3306)/gorm_test?parseTime=true&loc=Local&charset=utf8mb4"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			Colorful:                  true,
+			IgnoreRecordNotFoundError: false,
+			LogLevel:                  logger.Info,
+		})
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.Create(&User{})
+}
