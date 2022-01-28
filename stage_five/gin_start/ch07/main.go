42f019a712f7223bc5307a500d248f970e7d6082
SeasonPilot <pilot1@foxmail.com>
2022-01-28 14:39:59 +0800

    feat: 练习: 1-8 注册表单的验证

--- stage_five/gin_start/ch07/main.go
+++ stage_five/gin_start/ch07/main.go
@@ -12,6 +12,14 @@ type LoginForm struct {
 	Password string `form:"password" json:"password" binding:"required"`
 }
 
+type SignUpForm struct {
+	Age        uint8  `json:"age" binding:"gte=1,lte=130"` // uint8 类型
+	Name       string `json:"name"  binding:"required,min=3"`
+	Email      string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required"`
+	RePassword string `json:"re_password" binding:"required,eqfield=Password"`
+}
+
 func main() {
 	r := gin.Default()
 
@@ -24,6 +32,20 @@ func main() {
 			return
 		}
 		c.JSON(http.StatusOK, gin.H{"msg": "登陆成功"})
+	})
+
+	r.POST("/signUp", func(c *gin.Context) {
+		err := c.ShouldBind(&SignUpForm{})
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "注册成功",
+		})
 	})
 
 	_ = r.Run()
