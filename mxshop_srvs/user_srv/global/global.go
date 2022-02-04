e24f497493f3494edc4942b41f8906963b479461
SeasonPilot <pilot1@foxmail.com>
2022-02-04 23:04:05 +0800

    feat: 练习: 1-6 用户列表接口

---
+++ mxshop_srvs/user_srv/global/global.go
@@ -0,0 +1,45 @@
+package global
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"development/mxshop_srvs/user_srv/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+var DB *gorm.DB
+
+func init() {
+	dsn := "golang:golang@2020@tcp(172.19.30.30:3306)/mxshop_user_srv?parseTime=true&loc=Local&charset=utf8mb4"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			Colorful:                  true,
+			IgnoreRecordNotFoundError: false,
+			LogLevel:                  logger.Info,
+		})
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = DB.AutoMigrate(&model.User{})
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ mxshop_srvs/user_srv/handler/user.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"context"
+
+	"development/mxshop_srvs/user_srv/global"
+	"development/mxshop_srvs/user_srv/model"
+	"development/mxshop_srvs/user_srv/proto"
+
+	"gorm.io/gorm"
+)
+
+type UserServer struct{}
+
+func modelToResp(user model.User) *proto.UserInfoResponse {
+	//在 grpc 的 message 中字段有默认值，你不能随便赋值 nil 进去，容易出错
+	//这里要搞清， 哪些字段是有默认值
+	userInfoResp := &proto.UserInfoResponse{
+		Id:       uint32(user.ID),
+		Password: user.Password,
+		Mobile:   user.Mobile,
+		NickName: user.NickName,
+		Gender:   user.Gender,
+		Role:     uint32(user.Role),
+	}
+	if user.Birthday != nil {
+		userInfoResp.BirthDay = uint32(user.Birthday.Unix())
+	}
+	return userInfoResp
+}
+
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+func (s *UserServer) GetUserList(ctx context.Context, in *proto.PageInfo) (resp *proto.UserListResponse, err error) {
+	var users []model.User
+	result := global.DB.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	resp.Total = int32(result.RowsAffected)
+
+	global.DB.Scopes(Paginate(int(in.Pn), int(in.PSize))).Find(&users)
+	for _, user := range users {
+		resp.Data = append(resp.Data, modelToResp(user))
+	}
+
+	return resp, nil
+}
