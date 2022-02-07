51fd38067b7be5e989aad4e414d8bf458e9e5b85
SeasonPilot <pilot1@foxmail.com>
2022-02-07 11:28:39 +0800

    feat: 练习: 1-11 批量生产用户

--- mxshop_srvs/user_srv/main.go
+++ mxshop_srvs/user_srv/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"crypto/sha512"
 	"flag"
 	"fmt"
 	"net"
 
+	"development/mxshop_srvs/user_srv/global"
 	"development/mxshop_srvs/user_srv/handler"
+	"development/mxshop_srvs/user_srv/model"
 	"development/mxshop_srvs/user_srv/proto"
 
+	"github.com/anaskhan96/go-password-encoder"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	//密码加密
+	options := &password.Options{SaltLen: 16, Iterations: 100, KeyLen: 32, HashFunction: sha512.New}
+	salt, encodedPwd := password.Encode("admin123", options)
+	pw := fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd)
+	fmt.Println(pw)
+
+	for i := 0; i < 10; i++ {
+		user := model.User{
+			Mobile:   fmt.Sprintf("1879876789%d", i),
+			Password: pw,
+			NickName: fmt.Sprintf("bobby%d", i),
+		}
+		global.DB.Save(&user)
+	}
+
 	ip := flag.String("ip", "0.0.0.0", "ip 地址")
 	port := flag.Int("port", 50051, "端口号")
 
