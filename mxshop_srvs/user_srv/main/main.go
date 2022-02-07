0fe82b0905fe94d03b6dd11d0af23d87c91ac2c1
SeasonPilot <pilot1@foxmail.com>
2022-02-07 10:32:27 +0800

    feat: 练习: 1-10 通过flag启动grpc服务

--- mxshop_srvs/user_srv/main/main.go
+++ mxshop_srvs/user_srv/main/main.go
@@ -1,41 +1,33 @@
 package main
 
 import (
-	"log"
-	"os"
-	"time"
+	"flag"
+	"fmt"
+	"net"
 
-	"development/mxshop_srvs/user_srv/model"
+	"development/mxshop_srvs/user_srv/handler"
+	"development/mxshop_srvs/user_srv/proto"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"gorm.io/gorm/schema"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	dsn := "golang:golang@2020@tcp(172.19.30.30:3306)/mxshop_user_srv?parseTime=true&loc=Local&charset=utf8mb4"
+	ip := flag.String("ip", "0.0.0.0", "ip 地址")
+	port := flag.Int("port", 50051, "端口号")
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			SlowThreshold:             time.Second,
-			Colorful:                  true,
-			IgnoreRecordNotFoundError: false,
-			LogLevel:                  logger.Info,
-		})
+	flag.Parse()
+	fmt.Println(*ip, *port)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: newLogger,
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	})
+	// 注册服务
+	g := grpc.NewServer()
+	proto.RegisterUserServer(g, &handler.UserServer{})
+
+	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *ip, *port))
 	if err != nil {
 		panic(err)
 	}
-
-	err = db.AutoMigrate(&model.User{})
+	// 启动服务
+	err = g.Serve(listen)
 	if err != nil {
 		panic(err)
 	}
