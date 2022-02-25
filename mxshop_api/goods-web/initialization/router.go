2116587c93e90a1f7043f85ced8ba6c20091b818
SeasonPilot <pilot1@foxmail.com>
2022-02-25 15:40:05 +0800

    feat: 练习: 1-11 轮播图接口和yapi的快速测试

--- mxshop_api/goods-web/initialization/router.go
+++ mxshop_api/goods-web/initialization/router.go
@@ -17,6 +17,7 @@ func Routers() *gin.Engine {
 	ApiGroup := Router.Group("/v1")
 	router.InitGoodsRouter(ApiGroup)
 	router.InitCategoryRouter(ApiGroup)
+	router.InitBannerRouter(ApiGroup)
 
 	return Router
 }
