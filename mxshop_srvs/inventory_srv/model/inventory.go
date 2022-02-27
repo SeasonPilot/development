ddb0bf589843ddcf2225b58529cfb6a519bccdd7
SeasonPilot <pilot1@foxmail.com>
2022-02-27 11:48:00 +0800

    feat: 练习: 1-2 表结构设计

--- mxshop_srvs/inventory_srv/model/inventory.go
+++ mxshop_srvs/inventory_srv/model/inventory.go
@@ -1,4 +1,8 @@
 package model
 
-type Category struct {
+type Inventory struct {
+	BaseModel
+	Goods   int32 `gorm:"type:int;index"` // 经常查询的要加索引
+	Stocks  int32 `gorm:"type:int"`
+	Version int32 `gorm:"type:int"` //分布式锁的乐观锁
 }
