d0e543c90dbecb9cae87aa074b58bb918c7a26a0
SeasonPilot <pilot1@foxmail.com>
2022-03-05 21:50:31 +0800

    feat: 练习: 2-5 订单列表页

--- mxshop_api/order-web/api/order/order.go
+++ mxshop_api/order-web/api/order/order.go
@@ -1,9 +1,71 @@
 package order
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"mxshop-api/order-web/api"
+	"mxshop-api/order-web/global"
+	"mxshop-api/order-web/models"
+	"mxshop-api/order-web/proto"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
 
 func List(c *gin.Context) {
+	userId, _ := c.Get("userId")
+	claims, _ := c.Get("claims")
 
+	customClaims, ok := claims.(*models.CustomClaims)
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	req := &proto.OrderFilterRequest{}
+	if customClaims.AuthorityID == 1 {
+		req.UserId = int32(userId.(uint))
+	}
+
+	pages := c.DefaultQuery("p", "0")
+	pagesInt, _ := strconv.Atoi(pages)
+	req.Pages = int32(pagesInt)
+
+	perNums := c.DefaultQuery("pnum", "0")
+	perNumsInt, _ := strconv.Atoi(perNums)
+	req.PagePerNums = int32(perNumsInt)
+
+	orderListResp, err := global.OrderClient.OrderList(c, req)
+	if err != nil {
+		zap.S().Errorw("获取订单列表失败")
+		api.RpcErrToHttpErr(c, err)
+		return
+	}
+
+	rspMap := make([]interface{}, 0)
+	for _, item := range orderListResp.Data {
+		tmpMap := make(map[string]interface{})
+
+		tmpMap["userId"] = item.Id
+		tmpMap["status"] = item.Status
+		tmpMap["pay_type"] = item.PayType
+		tmpMap["user"] = item.UserId
+		tmpMap["post"] = item.Post
+		tmpMap["total"] = item.Total
+		tmpMap["address"] = item.Address
+		tmpMap["name"] = item.Name
+		tmpMap["mobile"] = item.Mobile
+		tmpMap["order_sn"] = item.OrderSn
+		tmpMap["add_time"] = item.AddTime
+
+		rspMap = append(rspMap, tmpMap)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"total": orderListResp.Total,
+		"data":  rspMap,
+	})
 }
 
 func New(c *gin.Context) {
