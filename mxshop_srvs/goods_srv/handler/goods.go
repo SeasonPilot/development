32b9d642082f29c8b3fb3db2cf1913020ac89e50
SeasonPilot <pilot1@foxmail.com>
2022-03-10 16:35:24 +0800

    feat: 练习: 2-11 如何通过mysql和es协作完成商品的查询-2

--- mxshop_srvs/goods_srv/handler/goods.go
+++ mxshop_srvs/goods_srv/handler/goods.go
@@ -2,12 +2,14 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"mxshop-srvs/goods_srv/global"
 	"mxshop-srvs/goods_srv/model"
 	"mxshop-srvs/goods_srv/proto"
 
+	"github.com/olivere/elastic/v7"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -49,23 +51,21 @@ type GoodsServer struct {
 }
 
 func (s *GoodsServer) GoodsList(c context.Context, req *proto.GoodsFilterRequest) (*proto.GoodsListResponse, error) {
-	localDB := global.DB.Model(&model.Goods{})
-
+	bq := elastic.NewBoolQuery()
 	if req.PriceMin > 0 {
-		// 不重新赋值 localDB 好像也没有问题？？
-		localDB = localDB.Where("shop_price >= ?", req.PriceMin)
+		bq.Filter(elastic.NewRangeQuery("shop_price").Gte(float32(req.PriceMin)))
 	}
 	if req.PriceMax > 0 {
-		localDB = localDB.Where("shop_price <= ?", req.PriceMax)
+		bq.Filter(elastic.NewRangeQuery("shop_price").Lte(req.PriceMax))
 	}
 	if req.IsHot {
-		localDB = localDB.Where("is_hot = true")
+		bq.Filter(elastic.NewTermQuery("is_hot", req.IsHot))
 	}
 	if req.IsNew {
-		localDB = localDB.Where("is_new = true")
+		bq.Filter(elastic.NewTermQuery("is_new", req.IsNew))
 	}
 	if req.IsTab {
-		localDB = localDB.Where("is_tab = true")
+		bq.Filter(elastic.NewTermQuery("is_tab", req.IsTab))
 	}
 	// 顶级分类
 	if req.TopCategory > 0 {
@@ -75,37 +75,83 @@ func (s *GoodsServer) GoodsList(c context.Context, req *proto.GoodsFilterRequest
 			return nil, status.Errorf(codes.NotFound, "商品分类不存在")
 		}
 
+		var q string
 		// 判断 顶级分类 Level
 		if category.Level == 1 {
-			// 查询 1 级分类 包含的所有商品
-			localDB = localDB.Where(fmt.Sprintf("category_id in ( select id from category where parent_category_id in (select id from category where parent_category_id = %d) )", category.ID))
+			q = fmt.Sprintf("select id from category where parent_category_id in (select id from category where parent_category_id = %d) ", category.ID)
 		} else if category.Level == 2 {
-			// 查询 2 级分类 包含的所有商品
-			localDB = localDB.Where(fmt.Sprintf("category_id in (select id from category where parent_category_id in (%d) )", category.ID))
+			q = fmt.Sprintf("select id from category where parent_category_id = %d ", category.ID)
 		} else if category.Level == 3 {
-			// 查询 3 级分类 包含的所有商品
-			localDB = localDB.Where(fmt.Sprintf("category_id in (%d)", category.ID))
+			q = fmt.Sprintf("select id from category where id = %d", category.ID)
 		}
+
+		type Result struct {
+			ID int32
+		}
+		var results []Result
+		// 在 MySQL 中查询所有 Category.ID
+		global.DB.Model(&model.Category{}).Raw(q).Scan(&results)
+
+		var categoryIDs []interface{}
+		for _, r := range results {
+			categoryIDs = append(categoryIDs, r.ID)
+		}
+
+		// 拿着查出来的所有 Category.ID 在 ES 中查询 所有 goodsIDs
+		bq.Filter(elastic.NewTermsQuery("category_id", categoryIDs...))
 	}
 
 	if req.KeyWords != "" {
-		// 字段名称要与数据库中的 column 一致，不是 struct 字段,LIKE 后面要加 %
-		localDB = localDB.Where("name LIKE ?", "%"+req.KeyWords+"%")
+		bq.Must(elastic.NewMultiMatchQuery(req.KeyWords, "name", "goods_brief"))
 	}
 	if req.Brand > 0 {
-		localDB = localDB.Where("brand_id = ?", req.Brand)
+		bq.Filter(elastic.NewTermQuery("brand_id", req.Brand))
+	}
+
+	// 分页
+	if req.Pages == 0 {
+		req.Pages = 1
+	}
+	switch {
+	case req.PagePerNums > 100:
+		req.PagePerNums = 100
+	case req.PagePerNums <= 0:
+		req.PagePerNums = 10
+	}
+
+	// 执行 ES 搜索, 得到 goods
+	goodsInES, err := global.EsClient.Search().Index(model.EsGoods{}.GetIndexName()).Query(bq).From(int(req.Pages)).Size(int(req.PagePerNums)).Do(c)
+	if err != nil {
+		return nil, err
+	}
+	if len(goodsInES.Hits.Hits) == 0 {
+		return nil, status.Errorf(codes.NotFound, "没有查询到相关商品")
 	}
 
 	rsp := new(proto.GoodsListResponse)
 
-	// 要在分页前计算 total
-	var count int64
-	localDB.Count(&count)
-	rsp.Total = int32(count)
+	// 要在分页前计算 total  fixme: 要再写一个不带分页 ES 查询语句吗？  这里 Total 应该一直是 req.PagePerNums ？？
+	rsp.Total = int32(goodsInES.TotalHits())
+
+	type Goods struct {
+		ID int32 `json:"id"`
+	}
+	var (
+		goodsES  Goods
+		goodsIDs []int32
+	)
+	for _, hit := range goodsInES.Hits.Hits {
+		err = json.Unmarshal(hit.Source, &goodsES)
+		if err != nil {
+			return nil, err
+		}
+		goodsIDs = append(goodsIDs, goodsES.ID)
+	}
 
 	var goods []model.Goods
-	//fixme: Brands 预加载不出来
-	result := localDB.Preload("Category").Preload("Brands").Scopes(Paginate(int(req.Pages), int(req.PagePerNums))).Find(&goods)
+	//fixme: Brands 预加载不出来,预加载全部 也不行
+	// goods 结果中 BrandsID=0 ？？  为什么？？  model.Goods.BrandsID  映射的字段名称与数据库不一致，所以查出来的 BrandsID=0
+	result := global.DB.Preload("Category").Preload("Brands").Find(&goods, goodsIDs)
 	if result.Error != nil {
 		return nil, result.Error
 	}
