25e1c006c8795cfddae87101c18d8651519a3adb
SeasonPilot <pilot1@foxmail.com>
2022-03-11 20:55:41 +0800

    feat: 练习: 2-4 go发送普通消息

---
+++ mxshop_srvs/order_srv/rocketmq_test/producer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
+)
+
+func main() {
+	p, err := rocketmq.NewProducer(producer.WithNameServer(primitive.NamesrvAddr{"172.19.30.30:9876"}))
+	if err != nil {
+		panic(err)
+	}
+
+	if err = p.Start(); err != nil {
+		panic(err)
+	}
+
+	resp, err := p.SendSync(context.Background(), &primitive.Message{
+		Topic: "imooc1",
+		Body:  []byte("hello word"),
+	})
+	if err != nil {
+		fmt.Printf("SendSync err: %s", err)
+		return
+	} else {
+		fmt.Println(resp.String())
+	}
+
+	if err = p.Shutdown(); err != nil {
+		fmt.Printf("Shutdown err: %s", err)
+	}
+}
