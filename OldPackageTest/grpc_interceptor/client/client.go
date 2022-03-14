2fca1634ce3bc10ce410c764f9a27e54192e8eb5
SeasonPilot <pilot1@foxmail.com>
2022-03-14 10:14:25 +0800

    feat: 练习: 4-4 使用grpc-middleware实现grpc调用的重试

--- OldPackageTest/grpc_interceptor/client/client.go
+++ OldPackageTest/grpc_interceptor/client/client.go
@@ -7,7 +7,9 @@ import (
 
 	"development/OldPackageTest/grpc_test/proto"
 
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 func main() {
@@ -19,9 +21,18 @@ func main() {
 		return err
 	}
 	opt := grpc.WithUnaryInterceptor(inter)
-
+	retryOpts := grpc.WithUnaryInterceptor(
+		grpc_retry.UnaryClientInterceptor(
+			// 重试次数
+			grpc_retry.WithMax(3),
+			// 超时时间
+			grpc_retry.WithPerRetryTimeout(2*time.Second),
+			// 设置应该重试哪些状态码
+			grpc_retry.WithCodes(codes.Unknown, codes.DeadlineExceeded, codes.Unavailable),
+		),
+	)
 	// 拨号
-	conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure(), opt)
+	conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure(), opt, retryOpts)
 	//grpc.Dial(":9950", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
