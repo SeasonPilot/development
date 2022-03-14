30f9600e611f27bec98bd93ba819d0ef452d89b6
SeasonPilot <pilot1@foxmail.com>
2022-03-14 22:04:19 +0800

    feat: 练习: 2-2 go发送多级嵌套span

--- OldPackageTest/jaeger_test/nesting_span.go
+++ OldPackageTest/jaeger_test/nesting_span.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
@@ -28,7 +29,17 @@ func main() {
 	}
 	defer closer.Close()
 
-	span := tracer.StartSpan("go-grpc-web")
+	parentSpan := tracer.StartSpan("main")
+
+	span := tracer.StartSpan("FunA", opentracing.ChildOf(parentSpan.Context()))
+	<-time.After(time.Millisecond * 500)
+	span.Finish()
+
+	<-time.After(time.Millisecond * 100)
+
+	span2 := tracer.StartSpan("FunB", opentracing.ChildOf(span.Context()))
 	<-time.After(time.Second)
-	defer span.Finish()
+	span2.Finish()
+
+	parentSpan.Finish()
 }
