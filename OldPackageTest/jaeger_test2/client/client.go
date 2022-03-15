c5dc4dec253c48a4bdc997ac36e2e00ce2038ad4
SeasonPilot <pilot1@foxmail.com>
2022-03-15 09:30:58 +0800

    fix: copy from jaeger_test

--- OldPackageTest/jaeger_test2/client/client.go
+++ OldPackageTest/jaeger_test2/client/client.go
@@ -4,14 +4,35 @@ import (
 	"context"
 	"fmt"
 
-	"google.golang.org/grpc/credentials/insecure"
-
 	"development/OldPackageTest/grpc_test/proto"
 
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+	cfg := jaegercfg.Configuration{
+		ServiceName: "mxshop",
+		// 采样
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			// 发送 span 到服务器时是否打印日志
+			LogSpans:           true,
+			LocalAgentHostPort: "172.19.30.31:6831",
+		},
+	}
+
+	tracer, closer, err := cfg.NewTracer(jaegercfg.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(err)
+	}
+	defer closer.Close()
+
 	// 拨号
 	conn, err := grpc.Dial("localhost:1234", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
